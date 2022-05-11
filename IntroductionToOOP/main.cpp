2ce100fba74bdb8cdb7f9b8fa2676cf3c9570122
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2022-05-11 22:11:52 +0300

    Class Point

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,22 +1,50 @@
-#include<iostream>
+п»ї#include<iostream>
 using namespace std;
-struct Point
+
+
+
+class Point
 {
 	double x;
 	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
 };
+//#define STRUCT_POINT
 void main()
 {
 	setlocale(LC_ALL, "");
+#ifdef STRUCT_POINT
 	//type name;
-	int a; //объявление пременной 'a' типа 'int'
-	Point A;//объявление пременной 'A' типа или структуры 'Point'
+	int a; //РѕР±СЉСЏРІР»РµРЅРёРµ РїСЂРµРјРµРЅРЅРѕР№ 'a' С‚РёРїР° 'int'
+	Point A;//РѕР±СЉСЏРІР»РµРЅРёРµ РїСЂРµРјРµРЅРЅРѕР№ 'A' С‚РёРїР° РёР»Рё СЃС‚СЂСѓРєС‚СѓСЂС‹ 'Point'
 	A.x = 2;
 	A.y = 3;
 	cout << A.x << "\t" << A.y << endl;
 
 	Point* pA = &A;
 	cout << pA->x << "\t" << pA->y << endl;
-	//вариант 2: cout << (*pA).x << "\t" << (*pA).y << endl;
+	//РІР°СЂРёР°РЅС‚ 2: cout << (*pA).x << "\t" << (*pA).y << endl;
 	cout << sizeof(Point) << endl;
+#endif
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
+
 }

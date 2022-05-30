42d9c21d0093c05c31143e0aab177a3fcd4682d8
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2022-05-30 17:52:10 +0300

    STRING HM

---
+++ String/Source.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+#define delimiter "\n-------------------------\n"
+class String
+{
+	int size;
+	char* str;
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
+	//                               Constructors
+	String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefaultConstructor:\t\t" << this << endl;
+	}
+	~String()
+	{
+		delete[] this->str;
+		cout << "Destructor:\t\t" << this << endl;
+	}
+	void print()
+	{
+		cout << "SIZE:\t" << size << endl;
+		cout << "STRING:\t" << str << endl;
+	}
+};
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	String str;
+	str.print();
+}
